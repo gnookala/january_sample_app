ea59b8be972da76483f806bcff620d159ad333ca
Gautham Nookala <gautham@integralsense.com>
2011-01-28 16:01:48 -0800

    User models with passwords and show page

--- Gemfile
+++ Gemfile
@@ -1,11 +1,10 @@
 source 'http://rubygems.org'
 
 gem 'rails', '3.0.3'
+gem 'gravatar_image_tag', '1.0.0.pre2'
 
 group :development do
   gem 'sqlite3-ruby', '1.3.2'
-  gem 'rspec-rails', '2.3.0'
-  gem 'annotate-models', '1.0.4'
 end
 
 group :test do
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,9 +1,10 @@
 class UsersController < ApplicationController
-  def show
-    @user = User.find(params[:id])
-  end
+	def show
+		@user = User.find(params[:id])
+		@title = @user.name
+	end
 
-  def new
-    @title = "Sign up"
-  end
+	def new
+		@title = "Sign up"
+	end
 end
